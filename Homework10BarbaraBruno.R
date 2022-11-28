71bda00d8ee5c431a2fbe975328953e1fc33c463
bbruno3 <bruno3@arizona.edu>
2022-11-28 22:09:24 +0000

    Test

---
+++ Homework10BarbaraBruno.R
@@ -0,0 +1,8 @@
+#Homework10
+#BE310
+#Barbara Bruno
+
+#install package
+install.packages("rmarkdown")
+
+#unzip r-novice-inflamation-data.zip
---
+++ data/patient-inflammation.R
@@ -0,0 +1,27 @@
+#Barbara Bruno
+#BE310
+#Homework 10
+
+
+package(readr)
+library(readr)
+library(here)
+library(tidyverse)
+
+git config --global user.email "bruno3@arizona.edu"
+
+data01 <- read_csv(file = "data/data/inflammation-01.csv")
+data01 <- read_csv(file = "data/data/inflammation-01.csv", header = FALSE, sep = ',')
+
+#r_novice_inflammation_data <- list.files(path = here("data", "data"), full.names = TRUE, pattern = "\\.csv$")
+
+
+#patient_inflammation <- read_csv(file = r_novice_inflammation_data)
+
+#patient-inflammation <- read_csv(file = r-novice-inflammation-data,
+                                # row_names=c("pat")
+                                # show_row_types = FALSE)
+
+
+
+
